53133179e5c9777e189e6aae9da570bc842cfc59
Jesús Armenta-Segura <138358307+JesusASmx@users.noreply.github.com>
2025-04-07 17:19:23 -0600

    Update macrofeatures.py

--- macrofeatures.py
+++ macrofeatures.py
@@ -1,1 +1,14 @@
-print("test")
+class embs1:
+  @staticmethod
+  def ola():
+    print("hola")
+
+class embs2:
+  @staticmethod
+  def ola():
+    print("hola")
+
+class embs3:
+  @staticmethod
+  def ola():
+    print("hola")
